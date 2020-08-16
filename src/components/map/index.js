b163fd38899999857ecff399a840814768a4df24
dewaqintoro97 <dewa.qintoro@students.amikom.ac.id>
2020-08-16 15:44:48 +0700

    v1

---
+++ src/components/map/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import PopupWithRedux from '../popup';
+
+const position = [53.8999964, 27.5666644];
+
+export default () => (
+  <div>
+    <Map center={position} zoom={13} maxZoom={18} style={{ height: 500 }}>
+      <TileLayer
+        url="http://tiles.maps.sputnik.ru/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          <PopupWithRedux />
+        </Popup>
+      </Marker>
+      <MarkerClusterGroup>
+        <Marker position={[53.910, 27.610]} />
+        <Marker position={[53.911, 27.611]} />
+        <Marker position={[53.912, 27.612]} />
+        <Marker position={[53.913, 27.613]} />
+      </MarkerClusterGroup>
+    </Map>
+  </div>
+);
